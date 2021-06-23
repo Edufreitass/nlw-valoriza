6f290dc512b6ccb12ad42225ffe0d80a1325a099
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-06-22 23:12:25 -0300

    Database and migrations config

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,8 @@
+import "reflect-metadata";
 import express, { response } from "express";
 
+import "./database"
+
 const app = express();
-
-app.get("/test", (request, response) => {
-  return response.send("Olá NLW");
-});
-
-app.post("/test-post", (request, response) => {
-  return response.send("Olá NLW método POST");
-});
 
 app.listen(3000, () => console.log("Server is running"));
