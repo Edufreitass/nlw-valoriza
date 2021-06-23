e4984f925d5e67d9339e229b89605301a25ab235
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-06-22 22:10:10 -0300

    Remove comments

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,7 @@ import express, { response } from "express";
 
 const app = express();
 
-/**
- * GET    => Buscar (Obter) uma informação
- * POST   => Inserir (Criar) uma informação
- * PUT    => Alterar (Atualizar) uma informação
- * DELETE => Remover (Apagar) uma informação
- * PATCH  => Alterar uma informação específica
- */
-
 app.get("/test", (request, response) => {
-  // Request => Entrando
-  // Response => Saindo
   return response.send("Olá NLW");
 });
 
@@ -20,5 +10,4 @@ app.post("/test-post", (request, response) => {
   return response.send("Olá NLW método POST");
 });
 
-// http://localhost:3000
 app.listen(3000, () => console.log("Server is running"));
