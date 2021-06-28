dcab30dc933b262086401a03033eb2284675c70f
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-06-27 22:37:11 -0300

    Adding a new tag as admin

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
 export function ensureAdmin(request: Request, response: Response, next: NextFunction) {
-  const admin = false;
+  const admin = true;
 
   if (admin) {
     return next();
