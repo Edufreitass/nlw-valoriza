bcbea8374e08c0fa54350738ed29c93375a0607f
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-06-28 22:57:52 -0300

    Create authenticate login route

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -2,13 +2,16 @@ import { Router } from "express";
 import { CreateTagController } from "../api/controllers/CreateTagController";
 import { CreateUserController } from "../api/controllers/CreateUserController";
 import { ensureAdmin } from "../api/middlewares/ensureAdmin";
+import { AuthenticateUserController } from "./controllers/AuthenticationUserController";
 
 const router = Router();
 
 const createUserController = new CreateUserController;
 const createTagController = new CreateTagController;
+const authenticateUserController = new AuthenticateUserController();
 
 router.post("/users", createUserController.handle);
 router.post("/tags", ensureAdmin, createTagController.handle);
+router.post("/login", authenticateUserController.handle);
 
 export { router };
