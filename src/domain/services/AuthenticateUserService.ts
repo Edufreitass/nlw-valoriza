be17b407262d36779ec33914ee454604fc7be783
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-06-28 22:50:33 -0300

    Create auth user service

---
+++ src/domain/services/AuthenticateUserService.ts
@@ -0,0 +1,39 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { getCustomRepository } from "typeorm"
+import { UsersRepositories } from "../../infrastructure/repositories/UsersRepositories"
+
+interface iAuthenticateRequest {
+  email: string;
+  password: string;
+}
+
+class AuthenticateUserService {
+  async execute({ email, password }: iAuthenticateRequest) {
+    const usersRepositories = getCustomRepository(UsersRepositories);
+
+    const user = await usersRepositories.findOne({
+      email
+    });
+
+    if (!user) {
+      throw new Error("Email/Password incorrect");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email/Password incorrect");
+    }
+
+    const token = sign(
+      { email: user.email },
+      "2ddbb1c5ac35a42dd2466f170b97aa21",
+      { subject: user.id, expiresIn: "1d" }
+    );
+
+    return token;
+  }
+}
+
+export { AuthenticateUserService }
