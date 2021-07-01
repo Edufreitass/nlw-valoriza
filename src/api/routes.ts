b2e4b742f29c63cd88332d820fbfd351c4019e57
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-06-30 22:53:17 -0300

    Create compliment route

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -3,15 +3,18 @@ import { CreateTagController } from "../api/controllers/CreateTagController";
 import { CreateUserController } from "../api/controllers/CreateUserController";
 import { ensureAdmin } from "../api/middlewares/ensureAdmin";
 import { AuthenticateUserController } from "./controllers/AuthenticationUserController";
+import { CreateComplimentController } from "./controllers/CreateComplimentController";
 
 const router = Router();
 
 const createUserController = new CreateUserController;
 const createTagController = new CreateTagController;
 const authenticateUserController = new AuthenticateUserController();
+const createComplimentController = new CreateComplimentController();
 
 router.post("/users", createUserController.handle);
 router.post("/tags", ensureAdmin, createTagController.handle);
 router.post("/login", authenticateUserController.handle);
+router.post("/compliments", createComplimentController.handle);
 
 export { router };
