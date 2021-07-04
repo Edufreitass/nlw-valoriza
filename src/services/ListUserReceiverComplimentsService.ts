25e2c8cd4d070b64ddf91e5931b83518282dc73d
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-07-04 16:21:50 -0300

    Getting user receiver relationships

--- src/services/ListUserReceiverComplimentsService.ts
+++ src/services/ListUserReceiverComplimentsService.ts
@@ -8,7 +8,12 @@ class ListUserReceiverComplimentsService {
     const compliments = await complimentsRepositories.find({
       where: {
         user_receiver: user_id
-      }
+      },
+      relations: [
+        "userSender",
+        "userReceiver",
+        "tag"
+      ]
     });
 
     return compliments;
