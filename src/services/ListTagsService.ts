701b9dadf90afed7c916d5871838250175972ef3
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-07-04 16:34:28 -0300

    Custom class using map()

--- src/services/ListTagsService.ts
+++ src/services/ListTagsService.ts
@@ -5,7 +5,8 @@ class ListTagsService {
   async execute() {
     const tagsRepositories = getCustomRepository(TagsRepositories);
 
-    const tags = await tagsRepositories.find();
+    let tags = await tagsRepositories.find();
+    tags = tags.map((tag) => ({ ...tag, nameCustom: `#${tag.name}` }));
 
     return tags;
   }
